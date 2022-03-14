4466b1fead2cee9f7a81844a51f18a12eea95f59
hgh-bibliophile <60857936+hgh-bibliophile@users.noreply.github.com>
2022-03-14 16:01:14 -0400

    Setting up PostgreSQL DB

--- db.py
+++ db.py
@@ -1,7 +1,12 @@
 import sqlalchemy
 import databases
 
-ORMAR_DATABASE_URL = "sqlite:///racetrack_api.db"
+import os
+
+POSTGRESQL_DATABASE_URL = os.environ.get('DATABASE_URL', False)
+SQLITE_DATABASE_URL = "sqlite:///racetrack_api.db"
+
+ORMAR_DATABASE_URL = SQLITE_DATABASE_URL if not  POSTGRESQL_DATABASE_URL else POSTGRESQL_DATABASE_URL
 
 metadata = sqlalchemy.MetaData()
 database = databases.Database(ORMAR_DATABASE_URL)
