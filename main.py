c9af4afb5cb0f814305b71411c1c0be7888333d3
hgh-bibliophile <60857936+hgh-bibliophile@users.noreply.github.com>
2022-03-14 21:10:16 -0400

    CORS Origins

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ import ormar
 app = FastAPI()
 
 origins = [
-    "http://localhost",
+    "http://api.racetrack.gratiafides.com",
+    "https://api.racetrack.gratiafides.com",
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
