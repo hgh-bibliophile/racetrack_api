8b450184cb7fdaab72514d29338482fc966d4612
hgh-bibliophile <60857936+hgh-bibliophile@users.noreply.github.com>
2022-03-14 22:21:46 -0400

    More CORSMiddleware

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from models import routers
 
 import ormar
 
-app = FastAPI()
+#app = FastAPI()
 
 origins = [
     "http://api.racetrack.gratiafides.com",
@@ -20,36 +20,56 @@ origins = [
     "http://127.0.0.1:3000",
 ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.state.database = database
-
-@app.on_event("startup")
-async def startup() -> None:
-    database_ = app.state.database
-    if not database_.is_connected:
-        await database_.connect()
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
-@app.on_event("shutdown")
-async def shutdown() -> None:
-    database_ = app.state.database
-    if database_.is_connected:
-        await database_.disconnect()
 
-app.include_router(routers.users)
-app.include_router(routers.tracks)
-app.include_router(routers.races)
-app.include_router(routers.cars)
-app.include_router(routers.lanes)
-app.include_router(routers.heats)
-app.include_router(routers.heat_runs)
+def create_app() -> CORSMiddleware:
+    """Create app wrapper to overcome middleware issues."""
+    app = FastAPI()
+    app.state.database = database
+
+    @app.on_event("startup")
+    async def startup() -> None:
+        database_ = app.state.database
+        if not database_.is_connected:
+            await database_.connect()
+
+
+    @app.on_event("shutdown")
+    async def shutdown() -> None:
+        database_ = app.state.database
+        if database_.is_connected:
+            await database_.disconnect()
+
+    app.include_router(routers.users)
+    app.include_router(routers.tracks)
+    app.include_router(routers.races)
+    app.include_router(routers.cars)
+    app.include_router(routers.lanes)
+    app.include_router(routers.heats)
+    app.include_router(routers.heat_runs)
+
+    return CORSMiddleware(
+        app,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+
+app = create_app()
+
+
+
+
 
 
 
